da01da6cd9810eaa26c3cc29e6f91b18c5ba140b
manumafe98 <manumaxerapro@gmail.com>
2025-07-17 00:23:43 -0300

    Add chainlink automation behavior to the contract

--- src/ClimateFetcher.sol
+++ src/ClimateFetcher.sol
@@ -4,29 +4,9 @@ pragma solidity ^0.8.30;
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// internal & private view & pure functions
-// external & public view & pure functions
-contract ClimateFetcher is FunctionsClient, ConfirmedOwner {
+contract ClimateFetcher is FunctionsClient, AutomationCompatibleInterface, ConfirmedOwner {
     error ClimateFetcher__UnexpectedRequestId(bytes32 requestId);
 
     using FunctionsRequest for FunctionsRequest.Request;
@@ -41,6 +21,10 @@ contract ClimateFetcher is FunctionsClient, ConfirmedOwner {
     string private s_weather;
     bytes private s_lastResponse;
     bytes private s_lastError;
+    string private constant DEFAULT_CITY = "Buenos Aires";
+
+    uint256 private s_lastTimeStamp;
+    uint256 private s_interval;
 
     string constant SOURCE = "const city = args[0];" "if (!secrets.SOLIDITY_API_KEY) {"
         "  throw Error('API key required');" "}" "const config = {"
@@ -55,16 +39,28 @@ contract ClimateFetcher is FunctionsClient, ConfirmedOwner {
         bytes32 donId,
         uint32 gasLimit,
         uint64 subscriptionId,
-        bytes memory encryptedSecretsUrls
+        bytes memory encryptedSecretsUrls,
+        uint256 interval
     ) FunctionsClient(router) ConfirmedOwner(msg.sender) {
         s_rounter = router;
         s_donId = donId;
         s_gasLimit = gasLimit;
         s_subscriptionId = subscriptionId;
         s_encryptedSecretsUrls = encryptedSecretsUrls;
+        s_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function sendRequest(string[] calldata args) external onlyOwner returns (bytes32 requestId) {
+    function performUpkeep(bytes calldata /* performData */ ) external override {
+        if ((block.timestamp - s_lastTimeStamp) > s_interval) {
+            s_lastTimeStamp = block.timestamp;
+            string[] memory args;
+            args[0] = DEFAULT_CITY;
+            sendRequest(args);
+        }
+    }
+
+    function sendRequest(string[] memory args) public onlyOwner returns (bytes32 requestId) {
         FunctionsRequest.Request memory req;
         req.initializeRequestForInlineJavaScript(SOURCE);
         if (args.length > 0) {
@@ -91,4 +87,19 @@ contract ClimateFetcher is FunctionsClient, ConfirmedOwner {
 
         emit Response(requestId, s_weather, s_lastResponse, s_lastError);
     }
+
+    function checkUpkeep(bytes calldata /* checkData */ )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /* performData */ )
+    {
+        upkeepNeeded = (block.timestamp - s_lastTimeStamp) > s_interval;
+        return (upkeepNeeded, "");
+    }
+
+    // Add deployer contract
+    // Add getter to check weather response
+    // Add testing with mocks -> FunctionsV1EventsMock
+    // Add chainlink Automation to automate the call of sendRequest
 }
